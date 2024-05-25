712b8b0c1bf0320ddbd428c4ac92af17dfabe766
Артем Андрієвський <andrievskija47@gmail.com>
2024-05-25 12:34:56 +0300

    мойо2

--- main.py
+++ main.py
@@ -1,10 +1,41 @@
+from kivymd.uix.backdrop.backdrop import MDCard
+from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivymd.uix.label import MDLabel
+from kivymd.uix.screen import MDScreen
+import requests
+from config import API_KEY, API_URL
+
+class WeatherCard(MDCard):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
 
 
-class MainApp(MDApp):
+
+class MainScreen(MDScreen):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def weather_search(self):
+            city = self.ids.city_field.text.strip().lower()
+            api_params = {
+                "q": city,
+                "appid": API_KEY
+            }
+            data = requests.get(API_URL,api_params)
+            response = data.json()
+            print(response)
+            description = response["weather"][0]["description"]
+            self.ids.weather_card.ids.label.text = description
+
+class WeatherApp(MDApp):
     def build(self):
-        return MDLabel(text="Hello, World", halign="center")
+        Builder.load_file('style.kv')
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Purple"
+        self.screen = MainScreen("main_screen")
+        return self.screen
 
 
-MainApp().run()
+WeatherApp().run()
